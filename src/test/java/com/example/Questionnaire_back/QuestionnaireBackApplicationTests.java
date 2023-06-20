982aebacf7a9e046fa5aebad07d6e4eee2640b18
SAM <milsontw@gmail.com>
2023-06-20 10:45:40 +0800

    init

---
+++ src/test/java/com/example/Questionnaire_back/QuestionnaireBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Questionnaire_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
